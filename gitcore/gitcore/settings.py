1b592e60aacd4be0f16f97b99e9354e49400c7de
성기동 <sgd0947@gmail.com>
2021-01-23 15:45:52 +0900

    feat: django Page Update

--- gitcore/gitcore/settings.py
+++ gitcore/gitcore/settings.py
@@ -133,5 +133,6 @@ CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://localhost"]
 AUTH_USER_MODEL = "users.User"
 
 REST_FRAMEWORK = {
-
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
